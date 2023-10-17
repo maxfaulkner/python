24e1b1adef848067f0552454cfdccfdf163048a8
Max Faulkner <faulknermax.98@gmail.com>
2023-10-17 18:02:55 -0400

    Create folder and file
    the game now works

---
+++ Python/GuessTheNumber/guessTheNumber.py
@@ -0,0 +1,17 @@
+import random
+
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    userNum = 0
+    while random_number != userNum:
+        userNum = int(input("Guess a number from 1 to {x}: "))
+        if userNum > random_number:
+            print("Too High, Try Again")
+        elif userNum < random_number:
+            print("Too Low, Try Again")
+    print ("YAY, You Win")
+
+    
+guess(10)
